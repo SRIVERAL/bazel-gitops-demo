3310e5292e6a3705d0b55f95f7035abdd301963c
Santiago Rivera L <santiago@catador.tech>
2026-04-17 20:14:56 -0500

    feat: instrument app with prometheus metrics

--- saludo/main.py
+++ saludo/main.py
@@ -1,14 +1,21 @@
-# saludo/main.py
-import requests
+import time
+from prometheus_client import start_http_server, Counter
 
-def decir_hola():
-    print("¡Hola Ekumen! Conectando con el mundo exterior...")
-    try:
-        response = requests.get("https://api.ipify.org?format=json")
-        ip = response.json()["ip"]
-        print(f"Mi IP pública según el contenedor es: {ip}")
-    except Exception as e:
-        print(f"No pude conectar: {e}")
+# Definimos la métrica: Un contador para los saludos
+# El primer parámetro es el nombre de la métrica en Prometheus
+SALUDOS_TOTAL = Counter('saludo_app_mensajes_total', 'Total de saludos emitidos por la app')
+
+def main():
+    # Iniciamos un servidor web en el puerto 8000 para exponer las métricas
+    # Prometheus vendrá a este puerto a "leer" los datos
+    start_http_server(8000)
+    print("Servidor de métricas iniciado en el puerto 8000")
+
+    while True:
+        print("¡Hola Ekumen desde K8s con seguridad y métricas!")
+        # Incrementamos el contador
+        SALUDOS_TOTAL.inc()
+        time.sleep(10)
 
 if __name__ == "__main__":
-    decir_hola()
+    main()
